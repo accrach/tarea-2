aa946e91658f41b423761fe485139bc90ba1e1ad
accrach <mgomez@usm.cl>
2024-05-20 16:52:40 -0400

    divicion, del archivo

--- tarea_2.cpp
+++ tarea_2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+#include<fstream>
+#include<string>
 using namespace std;
 
 
@@ -31,6 +33,47 @@ void limpiar(); // Se deben borrar todos los nodos del super-string
 
 };
 
+int main() {
+    ifstream archivo("prueba.txt");
+    string par1, par2, par3;
 
+    if (!archivo.is_open()) {
+        cerr << "Error al abrir el archivo" << endl;
+        return 1; // error
+    }
 
+    string linea;
+    while (getline(archivo, linea)) { // lee el archivo 
+        size_t pos1 = linea.find(' '); // Encuentra la posición del primer espacio en la línea
+        if (pos1 != string::npos) { // Verifica si se encontró un espacio
+            par1 = linea.substr(0, pos1); // Obtiene la primera parte
+            size_t pos2 = linea.find(' ', pos1 + 1); // Encuentra la posición del segundo espacio
+            if (pos2 != string::npos) { // Verifica si se encontró un segundo espacio
+                par2 = linea.substr(pos1 + 1, pos2 - pos1 - 1); // Obtiene la segunda parte
+                par3 = linea.substr(pos2 + 1); // Obtiene la tercera parte
+            } else {
+                // Si no se encontró un segundo espacio, solo hay dos partes
+                par2 = linea.substr(pos1 + 1); // Obtiene la segunda parte
+            }
 
+            // Verificar si la segunda parte contiene el nombre "El_Carlos"
+            size_t posElCarlos = par2.find("El_Carlos");
+            if (posElCarlos != string::npos) {
+                // Si la segunda parte contiene "El_Carlos", actualizarla
+                par2 = par2.substr(0, posElCarlos);
+            }
+
+            cout << "Par 1: " << par1 << endl;
+            cout << "Par 2: " << par2 << endl;
+            if (!par3.empty()) {
+                cout << "Par 3: " << par3 << endl;
+            }
+        } else {
+            // Si no se encuentra ningún espacio, hay solo una parte
+            cout << "Par único: " << linea << endl;
+        }
+    }
+
+    archivo.close(); // cierra el archivo después de terminar de leerlo
+    return 0;
+}
